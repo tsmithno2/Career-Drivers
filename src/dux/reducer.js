d10e19e2e35bc4986bee61bc3941e56d45fa7364
Trevor Smith <t.smithno2@gmail.com>
2019-01-16 19:00:00 -0700

    Begin setting up reducer for redux

--- src/dux/reducer.js
+++ src/dux/reducer.js
@@ -1,34 +1,8 @@
 const initState = {
   user: {},
-  campaignsList: [],
-  partyList: [],
-  newParty: []
+  answers: {}
 };
 
 const GET_USER_DATA = "GET_USER_DATA";
-const DISPLAY_CAMPAIGNS = "DISPLAY_CAMPAIGNS";
-const CREATE_CAMPAIGN = "CREATE_CAMPAIGN";
-const GET_PARTIES = "GET_PARTIES";
-const DELETE_CAMPAIGN = "DELETE_CAMPAIGN";
 
-export default function reducer(state = initState, action) {
-  switch (action.type) {
-    case GET_USER_DATA + "_FULFILLED":
-      return Object.assign({}, state, { user: action.payload });
-
-    case DISPLAY_CAMPAIGNS + "_FULFILLED":
-      return Object.assign({}, state, { campaignsList: action.payload });
-
-    case GET_PARTIES + "_FULFILLED":
-      return Object.assign({}, state, { partyList: action.payload });
-
-    case CREATE_CAMPAIGN + "_FULFILLED":
-      return Object.assign({}, state, { newPartyId: action.payload });
-
-    case DELETE_CAMPAIGN + "_FULFILLED":
-      return Object.assign({}, state, { campaignsList: action.payload });
-
-    default:
-      return state;
-  }
-}
+export default function reducer(state = initState, action) {}
