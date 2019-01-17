31278092a823ee47e3c3225ac05cff06e4884f62
Trevor Smith <t.smithno2@gmail.com>
2019-01-17 13:50:52 -0700

    Attributes correctly defined in local state. read comment for more info

--- src/components/resultsSection/quizResults/quizResults.js
+++ src/components/resultsSection/quizResults/quizResults.js
@@ -11,7 +11,33 @@ class QuizResults extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: ""
+      user: "",
+      //These are the definitions for each driver or attribute
+      //I'm not sure at this point if I will need these in this component or the graph one.
+      //So for now I'll copy and paste it there as well just to save myself some future work
+      entrepreneurial:
+        "Creating new things and championing new ideas, products, processes, and so on.",
+
+      autonomy:
+        "Having the freedom to control one’s own work methods, schedules, and outcomes; being self-reliant.",
+
+      idealistic:
+        "Being dedicated to a cause greater than oneself; giving of oneself in service to others.",
+
+      lifestyle:
+        "Achieving a certain lifestyle by finding ways to balance and integrate one’s personal needs with the needs of work and career.",
+
+      functionalTechnical:
+        "Engaging in work that utilizes one’s specific talents and education; doing work where one can be the expert.",
+
+      security:
+        "Needing security, stability, recognition, and predictability; demonstrating qualities of loyalty, dependability, and hard work.",
+
+      managerial:
+        "Being in roles where one can provide direction and leadership for others; climbing the managerial ladder.",
+
+      challenge:
+        "Doing things that represent ever increasing challenge; overcoming significant odds or winning against formidable competitors."
     };
   }
 
