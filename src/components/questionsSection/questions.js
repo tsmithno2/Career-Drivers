ab630c2917b937f0024053aaea86189a8439fd1e
Trevor Smith <t.smithno2@gmail.com>
2019-01-17 13:59:04 -0700

    Set up various objects in state to match redux state

--- src/components/questionsSection/questions.js
+++ src/components/questionsSection/questions.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-// import { connect } from "react-redux";
+import { connect } from "react-redux";
 // to import a function from the reducer and use redux use the following format
 // import { function name is the store } from "../../dux/reducer";
 //DONT FORGET TO IMPORT THE CSS STYLE SHEETS AS WELL
@@ -8,8 +8,8 @@ import { Link } from "react-router-dom";
 //READ FIRST!!!!!!!!!! i will be using redux to track all state values in final build, however right now i just have everything set to state. THIS WILL CHANGE!!!!!!!!!!!
 
 class Questions extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       //First: set of letters to track the different attributes amount
       e: 0,
@@ -20,36 +20,48 @@ class Questions extends Component {
       m: 0,
       c: 0,
       //SECOND: q is short for question and then the number is the question number.
-      // this will track answers for each question for the second phase of the test which is super selecting 2 answers
-      answers: {
-        q1: "",
-        q2: "",
-        q3: "",
-        q4: "",
-        q5: "",
-        q6: "",
-        q7: "",
-        q8: "",
-        q9: "",
-        q10: "",
-        q11: "",
-        q12: "",
-        q13: "",
-        q14: "",
-        q15: "",
-        q16: "",
-        q17: "",
-        q18: "",
-        q19: "",
-        q20: "",
-        q21: "",
-        q22: "",
-        q23: "",
-        q24: "",
-        q25: "",
-        q26: "",
-        q27: "",
-        q28: ""
+      //this will track answers for each question for the second phase of the test which is super selecting 2 answers
+      //Now it is named the same way as in redux
+      quizAnswers: {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: "",
+        6: "",
+        7: "",
+        8: "",
+        9: "",
+        10: "",
+        11: "",
+        12: "",
+        13: "",
+        14: "",
+        15: "",
+        16: "",
+        17: "",
+        18: "",
+        19: "",
+        20: "",
+        21: "",
+        22: "",
+        23: "",
+        24: "",
+        25: "",
+        26: "",
+        27: "",
+        28: ""
+      },
+      //THIRD: this is to track the quality counters based on answers, in the same format as redux
+      qualityCounters: {
+        e: "",
+        a: "",
+        i: "",
+        l: "",
+        fT: "",
+        s: "",
+        m: "",
+        c: ""
       }
     };
   }
@@ -67,4 +79,8 @@ class Questions extends Component {
   }
 }
 
-export default Questions;
+function mapStateToProps(state) {
+  return {
+    qualityCounters
+  };
+}
